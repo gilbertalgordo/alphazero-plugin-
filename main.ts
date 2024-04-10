e86f72f1f33ba8241b128c35a59c582aac889c6c
Gilbert Algordo <69397216+gilbertalgordo@users.noreply.github.com>
2024-04-10 12:53:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,15 @@ controller.player4.onEvent(ControllerEvent.Connected, function () {
     text_list = ["a", "b", "c"]
 })
 info.onScore(100, function () {
-	
+    controller.moveSprite(mySprite)
 })
 controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    animation.runMovementAnimation(
+    mySprite,
+    animation.animationPresets(animation.flyToCenter),
+    0,
+    true
+    )
     animation.runMovementAnimation(
     mySprite,
     animation.animationPresets(animation.flyToCenter),
@@ -38,6 +44,9 @@ mySprite,
 true
 )
 tiles.setWallAt(tiles.getTileLocation(0, 0), true)
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+music.play(music.randomizeSound(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear)), music.PlaybackMode.UntilDone)
+music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 if (true) {
     console.logValue("x", 0)
     mySprite += 1
